1d7f3ccfc84cf6c3850878c05760968aae70c0e8
mmrzax <mmrzax@gmail.com>
2026-06-18 00:24:14 +0330

    force black bg

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,3 +1,18 @@
+local function force_black_bg()
+  local hls = vim.api.nvim_get_hl(0, {})
+  for name, attrs in pairs(hls) do
+    if not attrs.link then
+      attrs.bg = 0x000000
+      pcall(vim.api.nvim_set_hl, 0, name, attrs)
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = force_black_bg,
+})
+
 return {
   {
     "rose-pine/neovim",
@@ -5,21 +20,18 @@ return {
     priority = 1000,
     config = function()
       require("rose-pine").setup({
-        variant = "main",
-        dark_variant = "main",
         styles = {
-          bold = true,
           italic = false,
-          transparency = false,
         },
       })
-      vim.cmd.colorscheme("rose-pine-main")
+      vim.cmd.colorscheme("rose-pine-moon")
+      force_black_bg()
     end,
   },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
-    },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "rose-pine",
+  --   },
+  -- },
 }
